0d2f0384a78c4db3ee96a8642f9794d1e8750cb6
sam <samueladeleke302@gmail.com>
2024-03-27 17:07:49 +0100

    completed the forgot password page

---
+++ JS/forgot.js
@@ -0,0 +1,14 @@
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup")
+}
+function closePopup(){
+    popup.classList.remove("open-popup")
+};
+
+const email = document.getElementById('email');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+})
