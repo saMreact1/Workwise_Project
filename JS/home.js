7fb90f6d92cd548536730a2fc77ec452a8f100b6
Juicedream <judexfrayo@gmail.com>
2024-04-05 09:37:26 +0100

    features/dashoboard
    Dashboard page added

---
+++ JS/home.js
@@ -0,0 +1,10 @@
+// Modal Popup code
+
+function toggle() {
+  const blur = document.getElementById('blur');
+  blur.classList.toggle('active');
+
+  var popUp = document.getElementById('popup');
+  popUp.classList.toggle('show');
+
+}
