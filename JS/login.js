2b3ce654284bb38c77365a1ac980da23f6ca518f
sam <samueladeleke302@gmail.com>
2024-04-06 09:54:12 +0100

    made some changes

--- JS/login.js
+++ JS/login.js
@@ -1,3 +1,7 @@
 const form = document.querySelector('form');
 const email = document.getElementById('email');
 const password = document.getElementById('pass');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+}
