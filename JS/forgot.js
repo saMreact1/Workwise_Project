8a45894272c390427302b5da3020e0f1a7b80c1c
sam <samueladeleke302@gmail.com>
2024-04-07 19:47:32 +0100

    made some changes

--- JS/forgot.js
+++ JS/forgot.js
@@ -10,5 +10,12 @@ function closePopup(){
 const email = document.getElementById('email');
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
+    var email_check = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    
+    if(!email.value.match(email_check)) {
+        e.preventDefault();
+        email_error.innerHTML = "Valid Email is required";
+    } else {
+        email_error.innerHTML = "";
+    }
 })
